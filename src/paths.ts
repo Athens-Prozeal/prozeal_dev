0225da943826f44137f658cdcc2b421084ff6d4b
Javid Rizwan M <85096675+JavidRizwan@users.noreply.github.com>
2024-07-20 22:46:39 +0530

    Sidebar dropdown menu

--- src/paths.ts
+++ src/paths.ts
@@ -1,14 +1,28 @@
+import { inspect } from 'util';
+
 export const paths = {
   home: '/',
-  auth: { signIn: '/auth/sign-in', signUp: '/auth/sign-up', resetPassword: '/auth/reset-password',  selectWorkSite: '/auth/select-work-site', },
+  auth: {
+    signIn: '/auth/sign-in',
+    signUp: '/auth/sign-up',
+    resetPassword: '/auth/reset-password',
+    selectWorkSite: '/auth/select-work-site',
+  },
   dashboard: {
     overview: '/dashboard',
     settings: '/dashboard/settings',
     account: '/dashboard/account',
   },
-  menu:{
-    toolboxtalk: '/menu/toolboxtalk',
-    manpower:'/menu/manpower',
+  menu: {
+    toolboxtalk: {
+      home: '/menu/toolboxtalk',
+      add: '/menu/toolboxtalk/add',
+      edit: '/menu/toolboxtalk/edit',
+    },
+    inspection: {
+      vehicle: '/menu/inspection/vehicle',
+    },
+    manpower: '/menu/manpower',
   },
 
   errors: { notFound: '/errors/not-found' },

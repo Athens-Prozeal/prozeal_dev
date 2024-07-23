7db373fa5e23eeecffcce6cbc51cc80b875ceef6
dcubixteam <dcubixteam@gmail.com>
2024-07-23 13:56:09 +0530

    Config role wise nav render

--- src/components/dashboard/layout/config.ts
+++ src/components/dashboard/layout/config.ts
@@ -1,21 +1,36 @@
 import type { NavItemConfig } from '@/types/nav';
 import { paths } from '@/paths';
 
-export const navItems = [
+let allNavItems: NavItemConfig[] = [
   { key: 'overview', title: 'Overview', href: paths.dashboard.overview, icon: 'chart-pie' },
   {
     key: 'toolboxtalk',
     title: 'Tool Box Talk',
     href: paths.menu.toolboxtalk.home,
-    icon: 'users-four',
-    matcher: { type: 'startsWith', href: paths.menu.toolboxtalk.home },
+    icon: 'tool-box',
+    matcher: { type: 'startsWith' as const, href: paths.menu.toolboxtalk.home },
   },
   { key: 'worker', title: 'Worker', href: paths.menu.worker, icon: 'worker' },
   {
     key: 'inspection',
     title: 'Vehicle Inspection',
-    icon: 'users-four',
+    icon: 'magnifying-glass',
     items: [{ key: 'vehicle', title: 'Vehicle', href: paths.menu.inspection.vehicle, icon: 'users-four' }],
   },
-  { key: 'manpower', title: 'Manpower', href: paths.menu.manpower, icon: 'users-four' },
-] satisfies NavItemConfig[];
+  { key: 'manpower', 
+    title: 'Manpower', 
+    href: paths.menu.manpower.home, 
+    icon: 'users-four',
+    matcher: { type: 'startsWith' as const, href: paths.menu.manpower.home },
+  },
+];
+
+const getNavItems = () => {
+  const role = localStorage.getItem('role');
+  if (role === 'sub_contractor') {
+    allNavItems = allNavItems.filter(item => item.key !== 'overview');
+  }
+  return allNavItems;
+};
+
+export const navItems: NavItemConfig[] = getNavItems();

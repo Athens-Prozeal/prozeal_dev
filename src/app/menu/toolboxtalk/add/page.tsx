e493fdcca8cb9aba610a1a424865858efac6fe80
dcubixteam <dcubixteam@gmail.com>
2024-07-31 19:45:46 +0530

    missed tbt add

--- src/app/menu/toolboxtalk/add/page.tsx
+++ src/app/menu/toolboxtalk/add/page.tsx
@@ -1,39 +1,23 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import { Stack, Typography } from '@mui/material';
-import axios from 'axios';
+import React, { useState } from 'react';
+import { Box, Button, FormControl, Grid, InputLabel, Stack, TextField, Typography } from '@mui/material';
 
-import { config } from '@/config';
+import ToolBoxTalkForm from '@/components/menu/toolboxtalk/add-form';
 
-export default function Page() {
-  const authToken = `Bearer ${localStorage.getItem('access-token')}`;
-  const [rowData, setRowData] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get(`${config.site.serverURL}/api/tbt/?work_site_id=${localStorage.getItem('work-site-id')}`, {
-        headers: {
-          Authorization: authToken,
-          'ngrok-skip-browser-warning': 'true',
-        },
-      })
-      .then((response) => {
-        setRowData(response.data);
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
-
+const Page = () => {
   return (
     <Stack spacing={3}>
       <Stack direction="row" spacing={3}>
-        <Stack spacing={1} sx={{ flex: '1 1 auto' }}>
+        <Stack spacing={5} sx={{ flex: '1 1 auto' }}>
           <Typography variant="h5">Add Tool Box Talk</Typography>
-          <Stack sx={{ alignItems: 'center' }} direction="row" spacing={1}></Stack>
+          <Box sx={{ flexGrow: 1 }}>
+            <ToolBoxTalkForm />
+          </Box>
         </Stack>
       </Stack>
     </Stack>
   );
-}
+};
+
+export default Page;

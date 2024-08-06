762dc8fd8412774c4de5433ce699082f06792446
sriramark <sriramarkk1@gmail.com>
2024-08-06 23:35:28 +0530

    Hard coded env

--- src/config.ts
+++ src/config.ts
@@ -6,16 +6,13 @@ export interface Config {
   logLevel: keyof typeof LogLevel;
 }
 
-console.log("process.env", process.env);
-console.log("process.env.serverURL", process.env.serverURL, process.env.NEXT_PUBLIC_LOG_LEVEL);
-
 export const config: Config = {
   site: {
     name: 'Athenas',
     description: 'EHS Software',
     themeColor: '#090a0b',
     url: getSiteURL(),
-    serverURL: process.env.serverURL ?? "http://localhost:3000",
+    serverURL: process.env.serverURL ?? "https://athenas-production.up.railway.app",
   },
   logLevel: (process.env.NEXT_PUBLIC_LOG_LEVEL as keyof typeof LogLevel) ?? LogLevel.ALL,
 };

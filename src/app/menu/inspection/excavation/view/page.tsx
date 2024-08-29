e0000c4108c7a082d86f9286ad09289e2210a0c9
Javid Rizwan M <85096675+JavidRizwan@users.noreply.github.com>
2024-08-29 22:57:47 +0530

    Excavation Grid overflow fixed  and add header

--- src/app/menu/inspection/excavation/view/page.tsx
+++ src/app/menu/inspection/excavation/view/page.tsx
@@ -1,17 +1,14 @@
 'use client';
 
 import { Box, Stack, Typography } from '@mui/material';
+
 import ExcavationDetail from '@/components/menu/inspection/excavation/view-detail';
 
 const Page = () => {
   return (
     <Stack spacing={3} sx={{ p: { xs: 2, sm: 3 } }}>
-      <Stack
-        direction={{ xs: 'column', sm: 'row' }}
-        spacing={3}
-        sx={{ flexWrap: 'wrap' }}
-      >
-        <Stack spacing={5} sx={{ flex: '1 1 auto' }}>
+      <Stack direction={{ xs: 'column', sm: 'row' }} spacing={3} sx={{ flexWrap: 'wrap' }}>
+        <Stack spacing={5} sx={{ flex: '1 1 auto', maxWidth: '100%' }}>
           <Typography variant="h5">Excavation Inspection Report</Typography>
           <Box sx={{ flexGrow: 1 }}>
             <ExcavationDetail />

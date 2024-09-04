efb72776b64c09110547412421bc78648db93c56
sriramark <sriramarkk1@gmail.com>
2024-09-05 02:57:47 +0530

    Typo tbt

--- src/app/menu/toolboxtalk/add/page.tsx
+++ src/app/menu/toolboxtalk/add/page.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import type { Metadata } from 'next';
 import { Box, Stack, Typography } from '@mui/material';
-import {config} from '@/config';
 
+import { config } from '@/config';
 import ToolBoxTalkForm from '@/components/menu/toolboxtalk/add-form';
 
-export const metadata = { title: `Tool Box Talk | Add | ${config.site.name}` } satisfies Metadata;
+export const metadata = { title: `Toolbox Talk | Add | ${config.site.name}` } satisfies Metadata;
 
 const Page = () => {
   return (
     <Stack spacing={3}>
       <Stack direction="row" spacing={3}>
         <Stack spacing={5} sx={{ flex: '1 1 auto' }}>
-          <Typography variant="h5">Add Tool Box Talk</Typography>
+          <Typography variant="h5">Add Toolbox Talk</Typography>
           <Box sx={{ flexGrow: 1 }}>
             <ToolBoxTalkForm />
           </Box>
--- src/app/menu/toolboxtalk/view/page.tsx
+++ src/app/menu/toolboxtalk/view/page.tsx
@@ -9,7 +9,7 @@ const Page = () => {
     <Stack spacing={3}>
       <Stack direction="row" spacing={3}>
         <Stack spacing={5} sx={{ flex: '1 1 auto' }}>
-          <Typography variant="h5">Tool Box Talk Report</Typography>
+          <Typography variant="h5">Toolbox Talk Report</Typography>
           <Box sx={{ flexGrow: 1 }}>
             <ToolBoxTalkDetail />
           </Box>
